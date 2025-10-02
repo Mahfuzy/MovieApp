78c489e21bf9d4f698eea98a6b569b8e0a0e2468
Mahfuzy <126839988+Mahfuzy@users.noreply.github.com>
2025-10-02 16:10:26 +0000

    Update AuthChecker.js

--- src/components/auth_components/AuthChecker.js
+++ src/components/auth_components/AuthChecker.js
@@ -1,7 +1,25 @@
-// AuthChecker.js
+import { useEffect, useState } from "react";
+import { auth } from "./Firebase";
+
 const AuthChecker = () => {
-  return null; // always pretend "no user"
-  // OR return { name: "Guest" }; // if you want a dummy user object
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                // User is signed in
+                setUser(authUser);
+            } else {
+                // User is signed out
+                setUser(null);
+            }
+        });
+
+        // Cleanup function to unsubscribe from the listener when the component unmounts
+        return () => unsubscribe();
+    }, []);
+
+    return user;
 };
 
 export default AuthChecker;
